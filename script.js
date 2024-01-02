6b9427f405286f36fc6c5de6dcf20233c0998c81
JerryCodesAfrica <egemonyeemelie@gmail.com>
2024-01-02 20:14:31 +0100

    done

---
+++ script.js
@@ -0,0 +1,62 @@
+
+let inputfields = document.getElementsByName("field");
+let error1 = document.querySelector(".error1");
+let error2 = document.querySelector(".error2");
+let error3 = document.querySelector(".error3");
+let output1 = document.querySelector(".outputMonth");
+let output2 = document.querySelector(".outputYear");
+let output3 = document.querySelector(".outputDays");
+
+//function to calculate the age
+//NB: this function will only be called when all validations are passed
+
+let displayErrors = () =>{
+    if (inputfields[0].value === " " || null) {
+        error1.style.display = "block";
+    } else {
+        error1.style.display = "block";
+    }
+    if (inputfields[1].value === " " || null) {
+        error2.style.display = "block";
+    } else {
+        error1.style.display = "block";
+    }
+    if (inputfields[2].value === " " || null) {
+        error3.style.display = "block";
+    } else {
+        error1.style.display = "none";
+    }
+}
+
+let calculate = () => {
+    const currentDate = new Date();
+
+    let day = inputfields[0].value;
+    let month = inputfields[1].value;
+    let year = inputfields[2].value;
+
+    const userBirthDate = new Date(`${month}/${day}/${year}`);
+    const timeDiff = Math.abs(currentDate - userBirthDate);
+
+    const day_output = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
+    const month_output = Math.floor(day_output / 30.44);
+    const year_output = Math.floor(day_output / 365.25);
+
+    output1.innerHTML = `${month_output}`;
+    output2.innerHTML = `${year_output}`;
+    output3.innerHTML = `${day_output}`;
+}
+
+
+
+//input validation
+//to check if the input fields are empty
+let checkAge = () => {
+   for (let i = 0; i < inputfields.length; i++) {
+    if (inputfields[i].value ===""|| null) {
+        displayErrors();
+    } else {
+        calculate();
+    }
+   }
+}
